47148d3582ecb871c19c07a82d16b00d95327414
hitoy <vip@hitoy.org>
2015-08-18 12:51:52 +0800

    update

--- admin_safe_ims/tools/update.php
+++ admin_safe_ims/tools/update.php
@@ -1,3 +1,5 @@
 <?php
-echo file_get_contents("http://lab.hitoy.org/msg/latestvertion.php");
+//获取最新版本信息
+$host=@$_SERVER["HTTP_HOST"];
+echo @file_get_contents("http://lab.hitoy.org/msg/latestvertion.php?host=$host");
 ?>
