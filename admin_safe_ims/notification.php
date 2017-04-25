bac092f2415a756fd1ba03d63a9c8137c2440a50
hitoy <vip@hitoy.org>
2017-04-25 14:26:21 +0800

    notification fixed

--- admin_safe_ims/notification.php
+++ admin_safe_ims/notification.php
@@ -6,7 +6,7 @@
 header("Content-Type:text/application");
 require_once("./init.php");
 $ignore=isset($_GET['ignore'])?trim($_GET['ignore'],","):'0';
-$mysql->setQuery("select * from ims_message where id not in($ignore) and msg_status = 0");
+$mysql->setQuery("select * from ims_message where id > $ignore and msg_status = 0");
 $list = $mysql->getRows();
 echo json_encode($list);
 
